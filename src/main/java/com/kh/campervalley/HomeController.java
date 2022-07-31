5e85c8afe688c67816464cb067e9ee92ac3d0615
haneulsky135@gmail.com <haneu@192.168.123.107>
2022-07-31 18:28:50 +0900

    TEST주석 삭제

--- src/main/java/com/kh/campervalley/HomeController.java
+++ src/main/java/com/kh/campervalley/HomeController.java
@@ -20,7 +20,6 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	// test
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
