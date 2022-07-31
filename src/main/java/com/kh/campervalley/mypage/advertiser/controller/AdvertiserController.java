d0eb66ef9ac4eef3c23f227b774e2a55d99e8ba5
venandi48 <bow999@naver.com>
2022-07-31 21:48:10 +0900

    220731 security+mapper설정 적용

--- src/main/java/com/kh/campervalley/mypage/advertiser/controller/AdvertiserController.java
+++ src/main/java/com/kh/campervalley/mypage/advertiser/controller/AdvertiserController.java
@@ -1,5 +1,6 @@
 package com.kh.campervalley.mypage.advertiser.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdvertiserController {
 	
+	@Autowired
 	private AdvertiserService advertiserService;
 	
 	@GetMapping("/register")
--- src/main/java/com/kh/campervalley/mypage/advertiser/model/service/AdvertiserServiceImpl.java
+++ src/main/java/com/kh/campervalley/mypage/advertiser/model/service/AdvertiserServiceImpl.java
@@ -1,8 +1,14 @@
 package com.kh.campervalley.mypage.advertiser.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.campervalley.mypage.advertiser.model.dao.AdvertiserDao;
 
 @Service
 public class AdvertiserServiceImpl implements AdvertiserService {
+
+	@Autowired
+	AdvertiserDao advertiserDao;
 
 }
