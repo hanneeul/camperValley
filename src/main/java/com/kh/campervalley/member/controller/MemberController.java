ee987da5a76b129a9ba880b87f4f0c9c50ce3204
haneulsky135@gmail.com <haneu@192.168.123.107>
2022-08-04 15:42:21 +0900

    주석 삭제 후 커밋

--- src/main/java/com/kh/campervalley/member/controller/MemberController.java
+++ src/main/java/com/kh/campervalley/member/controller/MemberController.java
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import com.kh.campervalley.member.model.dto.Member;
-//import com.kh.campervalley.member.model.service.MemberService;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
